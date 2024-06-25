f2ab052852bc4e46e68824a180760c7d2070991a
Bogdan.Rusu <81962952+bogdannrusu@users.noreply.github.com>
2024-06-25 11:19:04 +0300

    making login and wp component for personal users

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable react-refresh/only-export-components */
 import { lazy } from "react";
-import { RouteObject } from "react-router";
-
+import { RouteObject } from "react-router-dom";
 
 const Home = lazy(() => import("pages/Home"));
 const Goods = lazy(() => import("pages/GoodsNav"));
 const Login = lazy(() => import("pages/LoginNav"));
+const Wp = lazy(() => import("pages/WpNav"));
 
 const routes: RouteObject[] = [
     { path: "/", element: <Home /> },
     { path: "/goods", element: <Goods /> },
-    { path: "/login", element: <Login /> }
-
-    ];
+    { path: "/login", element: <Login /> },
+    { path: "/wp", element: <Wp /> }
+];
 
 export default routes;
